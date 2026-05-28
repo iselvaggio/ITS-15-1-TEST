bee242b169d770e1ea324f3e4b28ca8ce00a8943
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2026-05-28 09:56:34 +0200

    1. test

---
+++ ConsoleApp1/ConsoleApp1/Calcolatrice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calcolatrice
+    {
+        public double Somma(double a, double b)
+        {
+            return a + b;
+        }
+        public double Sottrazione(double a, double b)
+        {
+            return a - b;
+        }
+        public double Moltiplicazione(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divisione(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Non è possibile dividere per zero.");
+            }
+            return a / b;
+        }
+
+    }
+}
---
+++ ConsoleApp1/TestProject1/Test1.cs
@@ -0,0 +1,25 @@
+﻿
+using ConsoleApp1;
+namespace TestProject1
+{
+
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+                var calcolatrice = new ConsoleApp1.Calcolatrice();
+                double result = calcolatrice.Somma(2, 3);
+                Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+                var calcolatrice = new ConsoleApp1.Calcolatrice();
+                double result = calcolatrice.Sottrazione(5, 2);
+                Assert.AreEqual(3, result);
+        }
+
+    }
+}
